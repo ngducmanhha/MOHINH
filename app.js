5b1ae588617d0b9d09a0b97cd82128ad08f0a720
ngducmanhha <ngducmanhha@gmail.com>
2026-06-07 21:24:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-fetch("./data/data.json")
-.then(response => response.json())
-.then(data => {
+async function loadProducts(){
 
-    const html = data.PRODUCTS.map(product => `
+    const response = await fetch("./data/data.json");
+
+    const data = await response.json();
+
+    const productsContainer =
+        document.getElementById("products");
+
+    productsContainer.innerHTML =
+        data.PRODUCTS.map(product => `
+
         <div class="card">
 
-            <img src="${product.image_url}" alt="">
+            <img src="${product.image_url}">
 
-            <div class="card-body">
+            <div class="card-content">
 
                 <h3>${product.product_name}</h3>
 
@@ -18,16 +25,20 @@ fetch("./data/data.json")
                 </div>
 
                 <a
+                    class="buy-btn"
                     href="${product.affiliate_link}"
-                    target="_blank"
-                    class="buy-btn">
+                    target="_blank">
+
                     XEM GIÁ
+
                 </a>
 
             </div>
 
         </div>
+
     `).join("");
 
-    document.getElementById("products").innerHTML = html;
-});
+}
+
+loadProducts();
