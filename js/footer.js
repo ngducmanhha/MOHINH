87355421d4ec0fb1a0081eed2c3cfb350d68d5ab
ngducmanhha <ngducmanhha@gmail.com>
2026-07-11 18:24:14 +0700

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,39 @@
+function renderFooter(){
+
+return `
+
+<footer class="footer">
+
+<p>
+Mạnh Hà Mê Chơi Đồ là website tổng hợp deal mô hình,
+Bandai, phụ kiện và mã giảm giá.
+</p>
+
+<p>
+Website không bán hàng trực tiếp.
+</p>
+
+<p>
+
+<a href="gioithieu.html">
+Giới thiệu
+</a>
+
+|
+
+<a href="chinhsach.html">
+Chính sách
+</a>
+
+</p>
+
+<br><br>
+
+Mạnh Hà Mê Chơi Đồ - Website tổng hợp deal mô hình,
+Bandai, phụ kiện và mã giảm giá.
+
+</footer>
+
+`;
+
+}
