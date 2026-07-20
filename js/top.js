06befe738b493c146769350f7218d55fa13c2d85
ngducmanhha <ngducmanhha@gmail.com>
2026-07-20 07:54:39 +0700

    Update top.js

--- js/top.js
+++ js/top.js
@@ -1,6 +1,9 @@
 const TOP_TYPE =
-    new URLSearchParams(window.location.search).get("type");
-const PAGE_CONFIG={
-  page:"KITTRUNG",
-  sheet:"PRODUCTS"
+    new URLSearchParams(window.location.search).get("type") || "top101";
+
+window.IS_TOP_PAGE = true;
+
+const PAGE_CONFIG = {
+    page: "KITTRUNG",
+    sheet: "PRODUCTS"
 };
